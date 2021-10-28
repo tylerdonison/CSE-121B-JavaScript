cb26a974830387ecde8b7f10a34d872b7002256a
tylerdonison <86095868+tylerdonison@users.noreply.github.com>
2021-10-27 18:45:04 -0600

    Add files via upload

---
+++ Final-Sabrina Donison/scripts/task6.js
@@ -0,0 +1,5 @@
+//bonus for left section is +35 if left section score is 63 or over
+document.querySelector("#leftBonusScore").innerHTML = 10
+document.querySelector("#leftTotalScore").innerHTML = 10
+document.querySelector("#sixScore").innerHTML = 10
+document.querySelector("#grandTotalScore").innerHTML = 10
